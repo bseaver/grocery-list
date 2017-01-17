08421297050e578f53712ae3b3648c7a19fc9e81
Benjamin T. Seaver <BenjaminTSeaver@gmail.com>
2017-01-16 19:30:22 -0800

    complete sorting and upper casing and do slide up animation of result

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,22 @@ $(document).ready(function() {
 
 
   $("form").submit(function(event) {
-
     // Prevent page from refreshing
     event.preventDefault();
+
+    // Put input into upperCase, then split on line feeds, then filter out blanks
+    var uCGroceries = $("#inputGroceries").val().toUpperCase().split("\n").filter(function(entry) {
+      return /\S/.test(entry);
+    }).sort();
+
+    // Join back together with line feeds
+    $("#outputGroceries").text( uCGroceries.join("\n") );
+
+    // Hide input and show output
+    $("#inputSection").slideToggle();
+    $("#outputSection").slideToggle();
+
+
   }); // End form submit
 
 }); // End Document Ready JavaScript
